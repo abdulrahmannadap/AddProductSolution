ba7a9e7868b21520a5dcddfa33a1ed0ded8a68d7
abdul <abdul@NADAP>
2024-04-28 20:35:30 +0530

    Add project files.

---
+++ ProductCrud/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCrud.Models;
+
+namespace ProductCrud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
